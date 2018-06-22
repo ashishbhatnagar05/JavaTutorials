90cc7d803592919c6f12ee628f098eeeaed48079
ashishbhatnagar05 <ashishbhatnagar05@gmail.com>
2018-06-23 05:15:10 +0530

    added gitignore and spring rest project

---
+++ SpringRestLearning/src/main/java/com/spring/rest/SpringRestLearning/SpringRestLearningApplication.java
@@ -0,0 +1,12 @@
+package com.spring.rest.SpringRestLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestLearningApplication.class, args);
+	}
+}
