56fe6a6a6d45a54a5bc263aa1e4a754e261046cc
ashishbhatnagar05 <ashishbhatnagar05@gmail.com>
2018-09-12 01:40:35 +0530

    updated http request files for Intelij

--- SpringRestLearning/src/main/java/com/spring/rest/SpringRestLearning/SpringRestLearningApplication.java
+++ SpringRestLearning/src/main/java/com/spring/rest/SpringRestLearning/SpringRestLearningApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringRestLearningApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRestLearningApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestLearningApplication.class, args);
+    }
 }
--- SpringRestLearning/src/main/java/com/spring/rest/SpringRestLearning/controller/HelloController.java
+++ SpringRestLearning/src/main/java/com/spring/rest/SpringRestLearning/controller/HelloController.java
@@ -1,15 +1,41 @@
 package com.spring.rest.SpringRestLearning.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
 
-	@GetMapping("message")
-	public String getMessage() {
-		return "Hello World ";
-	}
+    @GetMapping("message")
+    public String getMessage() {
+        return "Hello World ";
+    }
+
+    @PostMapping("post")
+    public String post(@RequestBody Input input) {
+        System.out.println(input.getId() + input.getName());
+        return "Success";
+    }
+
+    static class Input {
+        private int id;
+        private String name;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
+
